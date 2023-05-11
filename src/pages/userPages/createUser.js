cb53c7c685eff43c9518aa5652eeabff3199c0e7
zafiro <ladvdgigi@gmail.com>
2023-05-11 17:40:59 -0500

    11 mayo 2

--- src/pages/userPages/createUser.js
+++ src/pages/userPages/createUser.js
@@ -67,12 +67,12 @@ const CreateUser = () => {
           <Form onSubmit={handleSubmit}>
             <Row>
               <Col md={6}>
-                <Form.Group controlId="firstName">
+              <Form.Group controlId="firstName">
                   <Form.Label>Nombres</Form.Label>
                   <Form.Control
                     type="text"
-                    name="title"
-                    value={formState.title}
+                    name="Nombres"
+                    value={formState.Nombres}
                     onChange={handleChange}
                   />
                 </Form.Group>
@@ -82,8 +82,8 @@ const CreateUser = () => {
                   <Form.Label>Apellidos</Form.Label>
                   <Form.Control
                     type="text"
-                    name="body"
-                    value={formState.body}
+                    name="Apellidos"
+                    value={formState.Apellidos}
                     onChange={handleChange}
                   />
                 </Form.Group>
@@ -93,11 +93,89 @@ const CreateUser = () => {
             <Form.Group controlId="email">
               <Form.Label>Correo Electronico</Form.Label>
               <Form.Control
-                type="number"
-                name="userId"
-                value={formState.userId}
+                type="email"
+                name="Correo"
+                value={formState.Correo}
                 onChange={handleChange}
               />
+            </Form.Group>
+
+            <Form.Group controlId="phone">
+              <Form.Label>Telefono</Form.Label>
+              <Form.Control
+                type="text"
+                name="Telefono"
+                value={formState.Telefono}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="user">
+              <Form.Label>Usuario</Form.Label>
+              <Form.Control
+                type="text"
+                name="Usuario"
+                value={formState.Usuario}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                name="Contraseña"
+                value={formState.Contraseña}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="company">
+              <Form.Label>Empresa</Form.Label>
+              <Form.Control
+                type="text"
+                name="Empresa"
+                value={formState.Empresa}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="identificationType">
+              <Form.Label>Tipo de Identificacion</Form.Label>
+              <Form.Control
+                as="select"
+                name="TipodeIdentificacion"
+                value={formState.TipodeIdentificacion}
+                onChange={handleChange}
+              >
+                <option value="CC">Cedula de Ciudadania</option>
+                <option value="CE">Cedula de Extranjeria</option>
+                <option value="NIT">NIT</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="identification">
+              <Form.Label>Identificacion</Form.Label>
+              <Form.Control
+
+                type="text"
+                name="Identificacion"
+                value={formState.Identificacion}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="status">
+              <Form.Label>Estado</Form.Label>
+              <Form.Control
+                as="select"
+                name="Estado"
+                value={formState.Estado}
+                onChange={handleChange}
+              >
+                <option value="Activo">Activo</option>
+                <option value="Inactivo">Inactivo</option>
+              </Form.Control>
             </Form.Group>
 
 
